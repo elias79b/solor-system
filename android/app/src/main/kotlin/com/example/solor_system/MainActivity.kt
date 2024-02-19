bfefc2b960a014eaa356d855e710cfe320aba8d5
elias79b <eliasbabakhani@gmail.com>
2024-02-19 20:39:26 +0330

    first commit

---
+++ android/app/src/main/kotlin/com/example/solor_system/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.solor_system
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:solor_system/screens/splash_screen.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: SplashScreen(),
+  ));
+}
+
